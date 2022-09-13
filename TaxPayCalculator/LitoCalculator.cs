4b484f95ac68853465f7a6fabfaad258b7afec76
chandra.dewi <chandra.dewi@zip.co>
2022-09-13 21:44:45 +1000

    Implementation Library Classes

--- TaxPayCalculator/LitoCalculator.cs
+++ TaxPayCalculator/LitoCalculator.cs
@@ -4,7 +4,15 @@
     {
         public decimal Calculate(Resident resident)
         {
-            throw new NotImplementedException();
+            var taxableIncome = resident.TaxableIncome;
+
+            if (taxableIncome >= 0 && taxableIncome <= 37500)
+                return 700;
+            if (taxableIncome >= 37501 && taxableIncome <= 45000)
+                return 700 - ((taxableIncome - 37500) * 0.05m);
+            if (taxableIncome >= 45001 && taxableIncome <= 66667)
+                return 325 - ((taxableIncome - 45000) * 0.015m);
+            return 0;
         }
     }
 }
--- TaxPayCalculator/LmitoCalculator.cs
+++ TaxPayCalculator/LmitoCalculator.cs
@@ -4,7 +4,24 @@
     {
         public decimal Calculate(Resident resident)
         {
-            throw new NotImplementedException();
+            var taxableIncome = resident.TaxableIncome;
+
+            if (taxableIncome >= 0 && taxableIncome <= 37000)
+                return 675;
+            if (taxableIncome >= 37001 && taxableIncome <= 48000)
+            {
+                var lmito = 675 + ((taxableIncome - 37000) * 0.075m); 
+                if (lmito > 1500)
+                    lmito = 1500;
+                return lmito;
+            }
+            if (taxableIncome >= 48000 && taxableIncome <= 90000)
+                return 1500;
+            if (taxableIncome >= 90001 && taxableIncome <= 126000)
+            {
+                return 1500 - ((taxableIncome - 90000) * 0.03m);
+            }
+            return 0;
         }
     }
 }
