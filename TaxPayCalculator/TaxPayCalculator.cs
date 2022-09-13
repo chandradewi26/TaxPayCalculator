7245604fd9bb2088b85eaa2c2274b6f9522d2609
chandra.dewi <chandra.dewi@zip.co>
2022-09-13 22:28:22 +1000

    TaxPayCalculator class

--- TaxPayCalculator/TaxPayCalculator.cs
+++ TaxPayCalculator/TaxPayCalculator.cs
@@ -5,7 +5,30 @@
     {
         public decimal Calculate(Resident resident)
         {
-            throw new NotImplementedException();
+
+            decimal GetCalculatedTax<TCalculator>()
+                where TCalculator : ICalculator, new()
+            {
+                return this.CalculateAmount<TCalculator>(resident);
+            }
+
+            var tax = GetCalculatedTax<TaxCalculator>();
+            var medicare = GetCalculatedTax<MedicareCalculator>();
+            var lito = GetCalculatedTax<LitoCalculator>();
+            var lmito = GetCalculatedTax<LmitoCalculator>();
+            var taxPayable = tax + medicare - lito - lmito;
+
+            if (taxPayable < 0)
+                return 0;
+
+            return taxPayable;
+        }
+
+        private decimal CalculateAmount<TCalculator>(Resident resident)
+            where TCalculator : ICalculator, new()
+        {
+            var calculator = new TCalculator();
+            return calculator.Calculate(resident);
         }
     }
 }
