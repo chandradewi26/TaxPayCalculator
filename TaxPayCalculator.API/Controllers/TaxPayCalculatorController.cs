7d47e6d0cdb4cde069956b46ab32627524c30728
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-09-16 03:21:54 +1000

    Added API

---
+++ TaxPayCalculator.API/Controllers/TaxPayCalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaxPayCalculator.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxPayCalculatorController : Controller
+    {
+            [HttpGet]
+            public IActionResult GetTaxResult(decimal income)
+            {
+                var _calculator = new TaxPayCalculator();
+
+                var _resident = new Resident(income);
+                var result = _calculator.Calculate(_resident);
+                return Ok(result);
+            }
+        }
+    }
