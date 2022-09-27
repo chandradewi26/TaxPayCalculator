d88e3580a03f6f0b378734a9aa8abc541909cc38
chandra.dewi <chandra.dewi@zip.co>
2022-09-27 10:55:10 +1000

    Refactor Tax Offset Threshold Provider

---
+++ TaxPayCalculator/LitoThresholdProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace TaxPayCalculator
+{
+    public class LitoThresholdProvider : ITaxOffsetThresholdProvider
+    {
+        public IList<TaxOffsetThreshold> CreateTaxOffsetThresholdTable()
+        {
+            var litoThresholdList = new List<TaxOffsetThreshold>();
+
+            litoThresholdList.Add(new TaxOffsetThreshold(0, 37500, 0m, 700));
+            litoThresholdList.Add(new TaxOffsetThreshold(37500, 45000, 0.05m, 700));
+            litoThresholdList.Add(new TaxOffsetThreshold(45000, 66667, 0.015m, 325));
+
+            return litoThresholdList;
+        }
+    }
+}
---
+++ TaxPayCalculator/LmitoThresholdProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace TaxPayCalculator
+{
+    public class LmitoThresholdProvider : ITaxOffsetThresholdProvider
+    {
+        public IList<TaxOffsetThreshold> CreateTaxOffsetThresholdTable()
+        {
+            var lmitoThresholdList = new List<TaxOffsetThreshold>();
+
+            lmitoThresholdList.Add(new TaxOffsetThreshold(0, 37000, 0m, 675));
+            lmitoThresholdList.Add(new TaxOffsetThreshold(37000, 48000, 0.075m, 675));
+            lmitoThresholdList.Add(new TaxOffsetThreshold(48000, 90000, 0m, 1500));
+            lmitoThresholdList.Add(new TaxOffsetThreshold(90000, 126000, 0.03m, 1500));
+
+            return lmitoThresholdList;
+        }
+    }
+}
